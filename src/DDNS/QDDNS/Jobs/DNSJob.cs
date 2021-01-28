8ad7de494f17470500606290d6a002f9c8bc3780
cardtfs\zhangwenxiang <qinqoushui@126.com>
2021-01-28 16:57:49 +0800

    重装系统前提交

---
+++ src/DDNS/QDDNS/Jobs/DNSJob.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QDDNS.Jobs
+{
+    public class DNSJob : QCommon.Service.Jobs.QuartzBase<string>
+    {
+        public SampleJob( ) : base(nameof(DNSJob), true)
+        {
+        }
+        protected override void doAfter(IList<string> data)
+        {
+
+        }
+
+        /// <summary>
+        /// 同步基础数据
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        protected override (string result, string error) doJob(string data)
+        {
+            //做业务
+            if (true)
+            {
+                return ("succ", "");
+            }
+            else
+                return ("fail", "");
+        }
+
+        protected override bool doPre()
+        {
+            return true;
+        }
+
+        protected override IList<string> getJobs(out int max, out int total)
+        {
+            var ss = new string[] { "DBJob" };
+            max = ss.Length;
+            total = ss.Length;
+            return ss;
+        }
+
+        protected override string getSubJobName(string data)
+        {
+            return data;
+        }
+    }
+}
---
+++ src/DDNS/QDDNS/Program.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace QDDNS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new ProgramHelper<Startup>().Run(nameof(QServiceDog), "服务守护狗", args,
+#if _Docker
+                        true
+#else
+                        false
+#endif
+                        ,
+#if DEBUG || IIS
+                        true
+#else
+                        false
+#endif
+    );
+        }
+    }
+    class ProgramHelper<TStartup> where TStartup : class
+    {
+        public void Run(string name, string desc, string[] args, bool docker = false, bool debug = false, string serviceTag = "service")
+        {
+
+            if (docker)
+            {
+                run(docker, args);
+            }
+            else
+            {
+                if (debug)
+                    run(docker, args);
+                else
+                    Q.Helper.ServiceHelper.StartService(Directory.GetCurrentDirectory(), name, desc, System.Reflection.Assembly.GetCallingAssembly(), () =>
+                    {
+                        run(docker, args);
+                    }, () => { run(docker, args); }, args, false, serviceTag, true);
+            }
+        }
+        void run(bool docker, string[] args)
+        {
+            if (docker)
+            {
+                //启动前检查文件并复制
+                string aPath = System.IO.Path.Combine(AppContext.BaseDirectory, "Bak");
+                string tPath = AppContext.BaseDirectory;
+                void copyFolder(DirectoryInfo source, DirectoryInfo target)
+                {
+                    foreach (DirectoryInfo dir in source.GetDirectories())
+                        copyFolder(dir, target.CreateSubdirectory(dir.Name));
+                    foreach (FileInfo file in source.GetFiles())
+                        if (!File.Exists(Path.Combine(target.FullName, file.Name)))
+                            file.CopyTo(Path.Combine(target.FullName, file.Name), false);
+                }
+                if (Directory.Exists(aPath))
+                {
+                    //尝试复制
+                    copyFolder(new DirectoryInfo(aPath), new DirectoryInfo(tPath));
+                }
+            }
+
+            System.Net.ServicePointManager.DefaultConnectionLimit = 512;
+            System.Threading.ThreadPool.SetMinThreads(25, 25);
+            System.Threading.ThreadPool.SetMaxThreads(100, 100);
+
+            var x = new Wi
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                x.Run();
+            }
+            else
+            {
+
+                if (args != null && args.FirstOrDefault(r => r.Equals("service", StringComparison.CurrentCultureIgnoreCase)) != null)
+                    x.RunAsService();
+                else
+                {
+                    x.Run();
+                }
+            }
+        }
+    }
