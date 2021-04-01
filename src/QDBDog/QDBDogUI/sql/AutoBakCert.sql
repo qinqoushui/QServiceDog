873cbec681121140f60cb24ffec6ec77d0e9a6dc
秦秋随 <qinqoushui@126.com>
2021-04-01 09:39:16 +0800

    证书安装脚本支持中文路径

--- src/QDBDog/QDBDogUI/sql/AutoBakCert.sql
+++ src/QDBDog/QDBDogUI/sql/AutoBakCert.sql
@@ -2,8 +2,8 @@
 CREATE MASTER KEY ENCRYPTION BY PASSWORD = 'ZyDBBak20200408';
 go
  
-CREATE CERTIFICATE AutoBakCert FROM FILE = '{0}\AutoBakCert.cer'
-WITH PRIVATE KEY (  FILE = '{0}\AutoBakCert.pkey', DECRYPTION  BY PASSWORD ='Cert123');
+CREATE CERTIFICATE AutoBakCert FROM FILE = N'{0}\AutoBakCert.cer'
+WITH PRIVATE KEY (  FILE = N'{0}\AutoBakCert.pkey', DECRYPTION  BY PASSWORD ='Cert123');
 go
 
 print N'启用证书和主控密钥完成';
